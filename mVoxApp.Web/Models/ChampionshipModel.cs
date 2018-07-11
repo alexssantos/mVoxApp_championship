680d4f056bf90e72fc46c60f78b92c4195078d7d
Alex Santos <arkanjo.alex@gmail.com>
2018-07-11 09:36:52 -0300

    Models em uma classe.

--- mVoxApp.Web/Models/ChampionshipModel.cs
+++ mVoxApp.Web/Models/ChampionshipModel.cs
@@ -1,18 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace mVoxApp.Web.Models
-{
-    public class ChampionshipModel
+{    
+    public class TeamModel
     {
         public int Id { get; set; }
-        public List<KeyGroupModel> ListKeyGroups { get; set; }
 
-        public ChampionshipModel()
-        {
-            ListKeyGroups = new List<KeyGroupModel>();
-        }
+        [Required]
+        [Display(Name = "Nome")]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        //Caso torneio comece por Pontos
+        [Required(ErrorMessage = "Informe o número do Grupo")]
+        [Display(Name = "Chave do Grupo")]
+        public int KeyGroup { get; set; }
+
+        public bool Winner { get; set; }
     }
+
+    public class MatchModel
+    {
+        public int Id { get; set; }
+        public string TeamA { get; set; }
+        public int GoalsTeamA { get; set; }
+        public string TeamB { get; set; }
+        public int GoalsTeamB { get; set; }
+    }
+
+    public class KeyGroupModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int MaxMatchs { get; set; }
+        public int TotalMatchs { get; set; }
+        List<MatchModel> ListMatchs { get; set; }
+    }
+
+    //public class ChampionshipModel
+    //{
+    //    public int Id { get; set; }
+    //    public List<KeyGroupModel> ListKeyGroups { get; set; }
+
+    //    public ChampionshipModel()
+    //    {
+    //        ListKeyGroups = new List<KeyGroupModel>();
+    //    }
+    //}
 }
