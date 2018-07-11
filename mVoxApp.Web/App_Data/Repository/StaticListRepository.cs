edbbc62765a6aaa19f6c227a69ad0d6d14ee4541
Alex Santos <arkanjo.alex@gmail.com>
2018-07-11 10:22:21 -0300

    Contrutor e PreLoad StaticList Criado

--- mVoxApp.Web/App_Data/Repository/StaticListRepository.cs
+++ mVoxApp.Web/App_Data/Repository/StaticListRepository.cs
@@ -9,26 +9,71 @@ using System.Data.SqlClient;
 namespace mVoxApp.Web.App_Data.Repository
 {
     public class StaticListRepository
-    {   
+    {
+        static List<TeamModel>      StaticListTeams;
         static List<KeyGroupModel>  StaticListGroups;
         static List<MatchModel>     StaticListMatch;
-        static List<TeamModel>      StaticListTeams;
 
         public StaticListRepository()
         {
             if (StaticListTeams == null)
             {
                 StaticListTeams = new List<TeamModel>();
+                PreLoadTeams();
             }
             if (StaticListGroups == null)
             {
                 StaticListGroups = new List<KeyGroupModel>();
+                PreLoadKeyGroups();
             }
             if (StaticListMatch == null)
             {
                 StaticListMatch = new List<MatchModel>();
             }
+        }
 
+
+        private void PreLoadTeams()
+        {
+            //Oitava, pelo menos, 16 times
+            for (int i = 0; i < 16; i++)
+            {
+                StaticListTeams.Add(
+                    new TeamModel { Id=i,
+                                    Name= $"Time {i}",
+                                    KeyGroup= 0,
+                                    Winner= false
+                    }
+                );
+            }
+        }
+
+        /// <KEYGroups>        
+        /// 0. Nenhum
+        /// 1. Oitavas
+        /// 2. Quartas
+        /// 3. Semi
+        /// 4. Finais
+        /// 
+        /// </KEYGroups>
+        private void PreLoadKeyGroups()
+        {
+            List<string> namesKeyGroups = new List<string>();
+            namesKeyGroups.Add("Não Qualificado"); 
+            namesKeyGroups.Add("Oitavas de Finais"); 
+            namesKeyGroups.Add("Quartas de Finais"); 
+            namesKeyGroups.Add("Quartas de Finais"); 
+            namesKeyGroups.Add("Final"); 
+
+            for (int i = 0; i < 5; i++)
+            {
+                StaticListGroups.Add(new KeyGroupModel { Id=i,
+                                                         Name = namesKeyGroups[i],
+                                                         MaxMatchs = ( 1/( 2^(i) ) ) * 32,
+                                                         TotalMatchs = 0 });
+            }
+
+            StaticListGroups[0].MaxMatchs = 1000;
         }
     }
 }
