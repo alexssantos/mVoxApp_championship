d8e8f03fcac27be64e92bb685eb56d08cf2fccc0
Alex Santos <arkanjo.alex@gmail.com>
2018-07-29 14:33:15 -0300

    Ass bootstrap theme

--- mVoxApp.Web/App_Start/BundleConfig.cs
+++ mVoxApp.Web/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace mVoxApp.Web
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-united-v3.css",
                       "~/Content/site.css"));
         }
     }
